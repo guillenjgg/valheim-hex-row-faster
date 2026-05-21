f5d03f8b8b66d0dbcefcb81d0448ffd1455749c4
devmail8181@gmail.com <devmail8181@gmail.com>
2026-05-21 12:20:48 -0600

    Initial project creation

---
+++ HexRowFaster/Plugin.cs
@@ -0,0 +1,39 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace HexRowFaster
+{
+    [BepInPlugin(PluginGuid, PluginName, PluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        const string PluginGuid = "com.hex.rowfaster";
+        const string PluginName = "HexRowFaster";
+        const string PluginVersion = "1.0.0";
+
+        internal static ManualLogSource Log;
+        internal static Plugin Instance;
+        internal static Harmony HarmonyInstance;
+
+        private void Awake()
+        {
+            Instance = this;
+
+            Log = Logger;
+
+            HarmonyInstance = new Harmony(PluginGuid);
+            HarmonyInstance.PatchAll();
+
+            Log.LogInfo($"Plugin {PluginName} v{PluginVersion} loaded.");
+        }
+
+        private void OnDestroy()
+        {
+            Log.LogInfo($"Plugin {PluginName} v{PluginVersion} unloaded.");
+
+            HarmonyInstance?.UnpatchSelf();
+            HarmonyInstance = null;
+            Instance = null;
+        }
+    }
+}
---
+++ HexRowFaster/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("HexRowFaster")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("HexRowFaster")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7a3b18f-1ccb-45d1-8f8c-344070327601")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
